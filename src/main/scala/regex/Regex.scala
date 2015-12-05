7bc02cec1b91d84238cb0a1174894fd5e07fb59d
Anthony Boccadoro <aboccadoro@umass.edu>
2015-12-05 01:46:18 -0500

    final submission

--- src/main/scala/regex/Regex.scala
+++ src/main/scala/regex/Regex.scala
@@ -3,11 +3,11 @@ package regex
 import scala.util.matching.Regex
 
 object EA06Regex {
-    val phone: Regex = """^[0-9]{3}[.|-][0-9]{3}[.|-][0-9]{4}$""".r
+    val phone: Regex = """^[0-9]{3}[\.|\-][0-9]{3}[\.|\-][0-9]{4}$""".r
 
     val email: Regex = """^.+@.+\.[a-zA-Z0-9]{3}$""".r
 
-    val advEmail: Regex = """^[^@]+@[^.|@]+\.[a-zA-Z0-9]{3}$""".r
+    val advEmail: Regex = """^[^@]+@[^\.|^@]+\.[a-zA-Z0-9]{3}$""".r
 
     val date: Regex = """^[0-9]{2}/[0-9]{2}/[0-9]{4}$""".r
 
